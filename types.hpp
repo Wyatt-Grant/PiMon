9935e32ff4e1024a40001755d0b2d6662e2bfb6a
Wyatt Grant <wyattgrant95@gmail.com>
2022-10-08 00:18:48 -0600

    main: added moves and stats

--- types.hpp
+++ types.hpp
@@ -40,8 +40,26 @@ const int32_t TARGET_ALL = 3;
 const int32_t TARGET_FIELD = 4;
 
 //move effects
-const int32_t NO_ATTACK = 0;
 const int32_t BASIC_ATTACK = 1;
+const int32_t INCREASED_CRIT_ATTACK = 2;
+const int32_t BURN_ATTACK = 4;
+const int32_t SLEEP_ATTACK = 5;
+const int32_t CONFUSION_ATTACK = 6;
+const int32_t RAISE_USER_SPEED = 7;
+const int32_t RAISE_USER_ATTACK = 8;
+const int32_t RAISE_USER_DEFENSE = 9;
+const int32_t RAISE_USER_SP_ATTACK = 10;
+const int32_t RAISE_USER_SP_DEFENSE = 11;
+const int32_t LOWER_ENEMY_SPEED = 12;
+const int32_t LOWER_ENEMY_ATTACK = 13;
+const int32_t LOWER_ENEMY_DEFENSE = 14;
+const int32_t LOWER_ENEMY_SP_ATTACK = 15;
+const int32_t LOWER_ENEMY_SP_DEFENSE = 16;
+
+// status effects
+const int32_t BURN_STATUS = 1;
+const int32_t SLEEP_STATUS = 2;
+const int32_t CONFUSE_STATUS = 3;
 
 struct move {
     int32_t id;
